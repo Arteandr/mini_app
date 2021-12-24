9d8b5f30a4ea7826cfee93af7ff04741e5fb9476
Arteandr <artedamatik@gmail.com>
2021-12-25 00:56:41 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import {
+	useAdaptivity,
+	AppRoot,
+	SplitLayout,
+	SplitCol,
+	ViewWidth,
+	View,
+	Panel,
+	PanelHeader,
+	Header,
+	Group,
+	SimpleCell,
+} from "@vkontakte/vkui";
+
+const App = () => {
+	const { viewWidth } = useAdaptivity();
+
+	return (
+		<AppRoot>
+			<SplitLayout header={<PanelHeader separator={false} />}>
+				<SplitCol spaced={viewWidth && viewWidth > ViewWidth.MOBILE}>
+					<View activePanel="main">
+						<Panel id="main">
+							<PanelHeader>VKUI</PanelHeader>
+							<Group header={<Header mode="secondary">Items</Header>}>
+								<SimpleCell>Hello</SimpleCell>
+								<SimpleCell>World</SimpleCell>
+							</Group>
+						</Panel>
+					</View>
+				</SplitCol>
+			</SplitLayout>
+		</AppRoot>
+	);
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AdaptivityProvider, ConfigProvider } from "@vkontakte/vkui"
+import "@vkontakte/vkui/dist/vkui.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<ConfigProvider>
+			<AdaptivityProvider>
+				<App />
+			</AdaptivityProvider>
+		</ConfigProvider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
